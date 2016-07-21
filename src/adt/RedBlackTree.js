28171974fdf4b2ee693b53be57d646bd896d72ac
Aurélien Ooms <aurelien.ooms@gmail.com>
2016-07-21 14:56:10 +0200

    use es6 classes

--- src/adt/RedBlackTree.js
+++ src/adt/RedBlackTree.js
@@ -7,62 +7,66 @@ import { delete_one_child } from '..' ;
 import { find } from '..' ;
 import { inordertraversal } from '..' ;
 
-export function RedBlackTree ( compare ) {
+export class RedBlackTree {
 
-	this.compare = compare ;
-	this.root = null ;
+	constructor ( compare ) {
+
+		this.compare = compare ;
+		this.root = null ;
+
+	}
+
+	add ( value ) {
+		if ( this.root === null ) {
+			this.root = new Node( BLACK , value ) ;
+		}
+		else {
+			const node = new Node( RED , value ) ;
+			insert( this.compare , this.root , node ) ;
+			insert_case2( node ) ;
+		}
+	}
+
+	find ( value ) {
+
+		if ( this.root === null ) return null ;
+
+		return find( this.compare , this.root , value ) ;
+
+	}
+
+	delete ( node ) {
+
+		if ( !node.left.isleaf() ) {
+			// replace node's value with predecessor's value
+			const pred = predecessor( node ) ;
+			node.value = pred.value ;
+			// delete predecessor node
+			// note: this node can only have one non-leaf child
+			//       because the tree is a red-black tree
+			delete_one_child( pred ) ;
+		}
+
+		else if ( !node.right.isleaf() ) {
+			// replace node's value with successor's value
+			const succ = successor( node ) ;
+			node.value = succ.value ;
+			// delete successor node
+			// note: this node can only have one non-leaf child
+			//       because the tree is a red-black tree
+			delete_one_child( pred ) ;
+		}
+
+		else {
+			this.root = null ;
+		}
+
+	}
+
+	*[Symbol.iterator] ( ) {
+
+		if ( this.root !== null ) yield* inordertraversal( this.root ) ;
+
+	}
 
 }
-
-RedBlackTree.prototype.add = function ( value ) {
-	if ( this.root === null ) {
-		this.root = new Node( BLACK , value ) ;
-	}
-	else {
-		const node = new Node( RED , value ) ;
-		insert( this.compare , this.root , node ) ;
-		insert_case2( node ) ;
-	}
-} ;
-
-RedBlackTree.prototype.find = function ( value ) {
-
-	if ( this.root === null ) return null ;
-
-	return find( this.compare , this.root , value ) ;
-
-} ;
-
-RedBlackTree.prototype.delete = function ( node ) {
-
-	if ( !node.left.isleaf() ) {
-		// replace node's value with predecessor's value
-		const pred = predecessor( node ) ;
-		node.value = pred.value ;
-		// delete predecessor node
-		// note: this node can only have one non-leaf child
-		//       because the tree is a red-black tree
-		delete_one_child( pred ) ;
-	}
-
-	else if ( !node.right.isleaf() ) {
-		// replace node's value with successor's value
-		const succ = successor( node ) ;
-		node.value = succ.value ;
-		// delete successor node
-		// note: this node can only have one non-leaf child
-		//       because the tree is a red-black tree
-		delete_one_child( pred ) ;
-	}
-
-	else {
-		this.root = null ;
-	}
-
-} ;
-
-RedBlackTree.prototype[Symbol.iterator] = function* ( ) {
-
-	if ( this.root !== null ) yield* inordertraversal( this.root ) ;
-
-} ;
