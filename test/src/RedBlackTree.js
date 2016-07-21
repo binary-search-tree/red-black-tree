9fbc2d2f952e8d09eddf5daa00bd34f9f579a3a6
Aurélien Ooms <aurelien.ooms@gmail.com>
2016-07-21 14:31:46 +0200

    more serious testing

--- test/src/RedBlackTree.js
+++ test/src/RedBlackTree.js
@@ -2,8 +2,6 @@ import test from 'ava' ;
 
 import { increasing , decreasing } from 'aureooms-js-compare' ;
 
-import { issorted } from 'aureooms-js-sort' ;
-
 import { list , range } from 'aureooms-js-itertools' ;
 
 import { randint } from 'aureooms-js-random' ;
@@ -19,14 +17,19 @@ test( 'RedBlackTree::Symbol.iterator' , t => {
 	t.deepEqual( a1.length , 0 , 'tree contains 0 elements' ) ;
 
 	const n = 10000 ;
+	const reference = [ ] ;
 
 	for ( let i of range( n ) ) {
-		tree.add( randint( 0 , n ) ) ;
+		const x = randint( 0 , n ) ;
+		tree.add( x ) ;
+		reference.push( x ) ;
 	}
+
+	reference.sort(increasing);
 
 	const a2 = list( tree ) ;
 
 	t.deepEqual( a2.length , n , `tree contains ${n} elements` ) ;
-	t.deepEqual( issorted( increasing , a2 , 0 , n ) , n , 'tree is sorted' ) ;
+	t.deepEqual( a2 , reference , 'tree is sorted' ) ;
 
 });
