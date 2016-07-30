d2db2e797383b32749ccd55b1da9234093108d3c
Aurélien Ooms <aurelien.ooms@gmail.com>
2016-07-30 20:49:31 +0200

    rename src/removal -> src/deletion

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 export * from './adt' ;
 export * from './color' ;
 export * from './debug' ;
+export * from './deletion' ;
 export * from './family' ;
 export * from './insertion' ;
-export * from './removal' ;
 export * from './rotate' ;
 export * from './search' ;
 export * from './traversal' ;
