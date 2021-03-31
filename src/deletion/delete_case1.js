64aaedcbe28712d58b2b065317fa9211ebfcce59
Aurélien Ooms <aurelien.ooms@gmail.com>
2021-03-31 16:52:15 +0200

    :recycle: refactor(delete_case1): Do not allow Leaf input.

--- src/deletion/delete_case1.js
+++ src/deletion/delete_case1.js
@@ -1,7 +1,6 @@
 import assert from 'assert';
 import BLACK from '../color/BLACK.js';
 import Node from '../types/Node.js';
-import Leaf from '../types/Leaf.js';
 import delete_case2 from './delete_case2.js';
 
 /**
@@ -10,10 +9,10 @@ import delete_case2 from './delete_case2.js';
  *   - all root-leaf paths going through n have a black height of b - 1
  *   - all other root-leaf paths have a black height of b
  *
- * @param {Node|Leaf} n - The input node.
+ * @param {Node} n - The input node.
  */
 const delete_case1 = (n) => {
-	assert(n instanceof Node || n instanceof Leaf);
+	assert(n instanceof Node);
 	assert(n._color === BLACK);
 	// If n is the root, there is nothing to do:
 	//   - all paths go through n, and
