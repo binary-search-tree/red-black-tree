6d8bd392a3d53168e21a2719daa3db1bdc07c3b8
Aurélien Ooms <aurelien.ooms@gmail.com>
2021-03-31 14:42:55 +0200

    :bicyclist: perf(delete_one_child): Hoist red input case.
    This avoids creating a mocked leaf in that case.

---
+++ src/deletion/prune_subtree.js
@@ -0,0 +1,18 @@
+import assert from 'assert';
+import Node from '../types/Node.js';
+import Leaf from '../types/Leaf.js';
+
+/**
+ * Prune subtree rooted at input node.
+ *
+ * @param {Node|Leaf} root - The leaf to delete.
+ */
+const prune_subtree = (root) => {
+	assert(root instanceof Node || root instanceof Leaf);
+	assert(root.parent !== null);
+
+	if (root === root.parent.left) root.parent.left = null;
+	else root.parent.right = null;
+};
+
+export default prune_subtree;
