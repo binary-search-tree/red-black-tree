9a8d62624f2c1dd8d65ba3adab013bc7e7c8c558
Aurélien Ooms <aurelien.ooms@gmail.com>
2021-03-31 16:25:21 +0200

    :recycle: refactor(delete): Split "no-child" and "exactly-one-child" cases.
    The coverage report seems to indicate that some additional pruning is
    possible in delete_one_child. Need to investigate.

--- src/deletion/delete_one_child.js
+++ src/deletion/delete_one_child.js
@@ -2,20 +2,18 @@ import assert from 'assert';
 import BLACK from '../color/BLACK.js';
 import RED from '../color/RED.js';
 import Node from '../types/Node.js';
-import Leaf from '../types/Leaf.js';
 
 import replace_node from './replace_node.js';
 import delete_case2 from './delete_case2.js';
 
-import prune_subtree from './prune_subtree.js';
-
 /**
- * Delete a node <code>n</code> that has at most a single non-leaf child.
+ * Delete a node <code>n</code> with one non-leaf left child and one leaf right
+ * child.
  *
  * Precondition:
- *   - n has at most one non-leaf child.
+ *   - n has exactly one non-leaf child.
  *   - n is not the root
- *   - if n has a non-leaf child, then it is its left child.
+ *   - n's only non-leaf child is n's left child.
  *   - hence, n's right child is a leaf
  *
  * @param {Node} n - The node to delete.
@@ -23,47 +21,31 @@ import prune_subtree from './prune_subtree.js';
 const delete_one_child = (n) => {
 	assert(n instanceof Node);
 	assert(n.parent !== null);
-	// Precondition: n's right child is a leaf.
-	// The right child of n is always a LEAF because either n is a subtree
-	// predecessor or it is the only child of its parent by the red-black tree
-	// properties
+	assert(n.left instanceof Node);
 	assert(n.right === null);
 
-	if (n._color === RED) {
-		//    If n is red then its child can only be black. Replacing n with its
-		//    child suffices.
-		const child = n.left;
-		if (child === null) {
-			prune_subtree(n);
-		} else {
-			assert(child._color === BLACK);
-			replace_node(n, child);
-		}
+	const child = n.left;
 
-		return;
-	}
-
-	assert(n._color === BLACK);
-
-	// Mock leaf if there is no left child
-	const child = n.left === null ? new Leaf(null) : n.left;
-
-	// Replace n with its left child
+	// Replace n with its only child
 	replace_node(n, child);
 
-	// If n is black, deleting it reduces the black-height of every path going
-	// through it by 1.
-	// We can easily fix this when its left child is an
-	// internal red node: change the color of the left child to black and
-	// replace n with it.
-	if (child._color === RED) child._color = BLACK;
-	// Otherwise, there are more things to fix.
-	else {
-		delete_case2(child);
+	if (n._color === BLACK) {
+		// If n is black, deleting it reduces the black-height of every path going
+		// through it by 1.
+		// We can easily fix this when its only child is an
+		// internal red node: change the color of the child to black and
+		// replace n with it.
+		if (child._color === RED) child._color = BLACK;
+		// Otherwise, there are more things to fix.
+		else {
+			delete_case2(child);
+		}
+	} else {
+		assert(n._color === RED);
+		// If n is red then its child can only be black. Replacing n with its
+		// child suffices. This has already been done above.
+		assert(child._color === BLACK);
 	}
-
-	// Delete mocked leaf
-	if (child instanceof Leaf) prune_subtree(child);
 };
 
 export default delete_one_child;
