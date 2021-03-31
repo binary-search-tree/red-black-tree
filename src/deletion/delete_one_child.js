7383d38335785a7e55fb0614c856f281c18d428b
Aurélien Ooms <aurelien.ooms@gmail.com>
2021-03-31 16:40:03 +0200

    :recycle: refactor(delete_one_child): Remove impossible execution paths.
    Indeed the previous coverage report gave the right insight.

--- src/deletion/delete_one_child.js
+++ src/deletion/delete_one_child.js
@@ -4,7 +4,6 @@ import RED from '../color/RED.js';
 import Node from '../types/Node.js';
 
 import replace_node from './replace_node.js';
-import delete_case2 from './delete_case2.js';
 
 /**
  * Delete a node <code>n</code> with one non-leaf left child and one leaf right
@@ -15,37 +14,27 @@ import delete_case2 from './delete_case2.js';
  *   - n is not the root
  *   - n's only non-leaf child is n's left child.
  *   - hence, n's right child is a leaf
+ *   - hence, n's left child is RED
+ *   - hence, n is BLACK
  *
  * @param {Node} n - The node to delete.
  */
 const delete_one_child = (n) => {
 	assert(n instanceof Node);
+	assert(n._color === BLACK);
 	assert(n.parent !== null);
 	assert(n.left instanceof Node);
+	assert(n.left._color === RED);
 	assert(n.right === null);
 
 	const child = n.left;
-
-	// Replace n with its only child
+	// If n is black, deleting it reduces the black-height of every path going
+	// through it by 1.
+	// We can easily fix this when its only child is an
+	// internal RED node: change the color of the child to black and
+	// replace n with it.
 	replace_node(n, child);
-
-	if (n._color === BLACK) {
-		// If n is black, deleting it reduces the black-height of every path going
-		// through it by 1.
-		// We can easily fix this when its only child is an
-		// internal red node: change the color of the child to black and
-		// replace n with it.
-		if (child._color === RED) child._color = BLACK;
-		// Otherwise, there are more things to fix.
-		else {
-			delete_case2(child);
-		}
-	} else {
-		assert(n._color === RED);
-		// If n is red then its child can only be black. Replacing n with its
-		// child suffices. This has already been done above.
-		assert(child._color === BLACK);
-	}
+	child._color = BLACK;
 };
 
 export default delete_one_child;
