23c5af4f0f4cd7b79c295cd1d2da4e858f27aa52
Aurélien Ooms <aurelien.ooms@gmail.com>
2021-04-01 16:58:44 +0200

    :microscope: test: Add simple edge cases to increase coverage.

---
+++ test/src/regression/deletion.js
@@ -0,0 +1,12 @@
+import test from 'ava';
+
+import {list} from '@aureooms/js-itertools';
+import {increasing} from '../../fixtures.js';
+import {RedBlackTree} from '../../../src/index.js';
+
+test('Edge case of small list', (t) => {
+	const reference = [5, 3, 2, 6, 7, 8];
+	const tree = RedBlackTree.from(increasing, reference);
+	t.true(tree.remove(3));
+	t.deepEqual(list(tree), [2, 5, 6, 7, 8]);
+});
