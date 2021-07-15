6a4a66dfd4110af618e472db0831c8816c351cea
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2021-07-15 15:22:46 +0200

    :recycle: refactor(api/from): Simplify.

--- src/api/from.js
+++ src/api/from.js
@@ -4,5 +4,4 @@ import RedBlackTree from '../types/RedBlackTree.js';
  * Same as {@link RedBlackTree.from}.
  * @function from
  */
-const from = RedBlackTree.from;
-export default from;
+export default RedBlackTree.from;
